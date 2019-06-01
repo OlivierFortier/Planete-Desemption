6b4c2d80f7e6b494f8bd466a2e6de9dd28a8228e
Olivier Fortier <olivier_fortier@outlook.com>
2019-06-01 14:24:04 -0400

    removed img viewer for the second time

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,7 +1,7 @@
 
 //fonction anonyme auto-exécutante qui initialise la page avec quelques écouteurs d'événements pour que l'afficheur d'image fonctionne correctement
 (function () {
-    
+    /*
     //initialisation des div pour le viewer d'images
     const petiteImage = document.querySelector("#imageDiapo");
     const grandeImage = document.querySelector("#iViewer img");
@@ -9,5 +9,5 @@
     //ajout des écouteurs d'événements pour le viewer d'image
     petiteImage.addEventListener("mouseup",afficheGrandeImage);
     grandeImage.addEventListener("mouseup",affichePetiteImage);
-    
+    */
 })();
